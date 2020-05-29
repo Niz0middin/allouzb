b7acef1d420418730544611c826268b000decfbd
Abdusaid Umarov <abdusaid.umarov.1996@gmail.com>
2020-05-29 10:35:46 +0500

    telegram bot integrated

---
+++ web/allouzbbot/keyboard-buttons.js
@@ -0,0 +1,23 @@
+module.exports = {
+    home:{
+        catalogs:'Каталог',
+        bin:'Корзина',
+        orders:'Заказы',
+        news:'Новости',
+        help:'Помощь'
+    },
+    help:{
+        call:'Позвонить',
+        write:'связаться с админом'
+    },
+    cancel:{
+        cancel:'Отмена'
+    },
+    back:{
+        backward:'Назад'
+    },
+    more:{
+        more:'Ещё'
+    }
+
+}
